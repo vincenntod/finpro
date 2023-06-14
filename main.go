7f5b81dd78260829d8dad52b174bd2618a8037fb
Vincencius Maxwell <vincennte@telkomuniversity.ac.id>
2023-06-14 15:07:36 +0700

    Route

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"golang/auth"
 	"golang/model"
+	"golang/module/account"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,9 +11,14 @@ import (
 func main() {
 	model.ConnectDatabase()
 	r := gin.Default()
-	// Admin := r.Group("/api", auth.MiddlewareAdmin)
+	Admin := r.Group("/api", auth.MiddlewareAdmin)
 	{
+		Admin.GET("/data-user", account.GetDataUser)
+		Admin.GET("/data-user/:id", account.GetDataUserById)
+		Admin.PUT("/data-user/:id", account.EditDataUser)
+		Admin.DELETE("/data-user/:id", account.DeleteDataUser)
 	}
-
+	r.POST("/create-user", account.CreateAccount)
+	r.POST("/login", account.Login)
 	r.Run(":8080")
 }
