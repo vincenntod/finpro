1517c86ad8e87092b138653ac2bcdbf0d6c4cc28
unknown <www.dwikky96@gmail.com>
2023-06-17 10:13:25 +0700

    refactoring usecase export csv

--- module/exportcsv/usecase.go
+++ module/exportcsv/usecase.go
@@ -16,44 +16,39 @@ func NewUseCase(repo *Repository) *UseCase {
 }
 
 func (u UseCase) ExportCSV(req *ExportCSVRequest) ([][]string, error) {
-	exportData := [][]string{[]string{"id", "od_number", "bank_acount_no", "billing_cycle_date", "payment_due_date",
-		"overflow_amount", "bill_amount", "principal_amount", "interest_amount", "total_fee_amount", "status", "payment_method",
-		"auto_debet_counter", "created_at", "updated_at", "is_hold", "is_fstl_pending", "is_hstl_pending", "is_laa_positif", "payment_amount",
-		"billing_gen_date", "is_oda_positif"}}
+
 	var resultGetTransaction []Transaction
+	var err error
 	switch {
-	case req.Status == "" && req.StartDate == "" && req.EndDate == "":
-		data, err := u.repo.GetAllTransaction()
-		if err != nil {
-			return nil, err
-		}
-		resultGetTransaction = data
-
-	case req.Status == "":
-		data, err := u.repo.GetAllTransactionByStartAndEndDate(req.StartDate, req.EndDate)
-		if err != nil {
-			return nil, err
-		}
-		resultGetTransaction = data
-
-	case req.StartDate == "" && req.EndDate == "":
-		data, err := u.repo.GetTransactionByStatus(req)
-		if err != nil {
-			return nil, err
-		}
-		resultGetTransaction = data
-	default:
-		data, err := u.repo.GetTransactionByStatusAndStartAndEndDate(req.Status, req.StartDate, req.EndDate)
-		if err != nil {
-			return nil, err
-		}
-
-		resultGetTransaction = data
+	case req.Status == "" && req.StartDate == "" && req.EndDate == "": //without filter
+		resultGetTransaction, err = u.repo.GetAllTransaction()
+	case req.Status == "": // rage date filter
+		resultGetTransaction, err = u.repo.GetAllTransactionByRangeDateFilter(req.StartDate, req.EndDate)
+	case req.StartDate == "" && req.EndDate == "": //only status filter
+		resultGetTransaction, err = u.repo.GetTransactionByStatusFilter(req)
+	default: //complate filter(status, rage date)
+		resultGetTransaction, err = u.repo.GetTransactionByStatusAndRangeDateFilter(req.Status, req.StartDate, req.EndDate)
 	}
+	//error handling
 	if len(resultGetTransaction) == 0 {
 		return nil, errors.New("Not Found")
 	}
-	for _, transaction := range resultGetTransaction {
+	if err != nil {
+		return nil, err
+	}
+
+	transactionStringData, err := TransactionStringConverter(resultGetTransaction)
+	return transactionStringData, nil
+
+}
+func TransactionStringConverter(transactions []Transaction) ([][]string, error) {
+	//set name field transaction in first record
+	stringData := [][]string{[]string{"id", "od_number", "bank_acount_no", "billing_cycle_date", "payment_due_date",
+		"overflow_amount", "bill_amount", "principal_amount", "interest_amount", "total_fee_amount", "status", "payment_method",
+		"auto_debet_counter", "created_at", "updated_at", "is_hold", "is_fstl_pending", "is_hstl_pending", "is_laa_positif", "payment_amount",
+		"billing_gen_date", "is_oda_positif"}}
+	//converting some data from transaction to string
+	for _, transaction := range transactions {
 		record := []string{strconv.Itoa(transaction.Id), transaction.Oda_number, transaction.Bank_account_no, transaction.Billing_cycle_date,
 			transaction.Payment_due_date.GoString(), strconv.FormatFloat(transaction.Overflow_amount, 'f', 6, 64), strconv.FormatFloat(transaction.Bill_amount, 'f', 6, 64),
 			strconv.FormatFloat(transaction.Principal_amount, 'f', 6, 64), strconv.FormatFloat(transaction.Interest_amount, 'f', 6, 64),
@@ -61,8 +56,8 @@ func (u UseCase) ExportCSV(req *ExportCSVRequest) ([][]string, error) {
 			transaction.Created_at.GoString(), transaction.Updated_at.GoString(), strconv.FormatBool(transaction.Is_hold), strconv.FormatBool(transaction.Is_fstl_pending),
 			strconv.FormatBool(transaction.Is_hstl_pending), strconv.FormatBool(transaction.Is_laa_positif), strconv.FormatFloat(transaction.Payment_amount, 'f', 6, 64),
 			transaction.Billing_gen_date.GoString(), strconv.FormatBool(transaction.Is_oda_positif)}
-		exportData = append(exportData, record)
+		stringData = append(stringData, record)
 	}
-	return exportData, nil
 
+	return stringData, nil
 }
