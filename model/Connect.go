90d0c6c91952e1404cd97482a3812a3a8337628a
Vincencius Maxwell <vincennte@telkomuniversity.ac.id>
2023-07-04 15:23:47 +0700

    edit port and cofig database

--- model/Connect.go
+++ model/Connect.go
@@ -11,7 +11,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "host=localhost user=postgres password=123456 dbname=dbceria port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := "host=containers-us-west-205.railway.app user=postgres password=A8MVAbgu58G5npdJzWQC dbname=railway port=6725 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
 		SingularTable: true,
 	}})
