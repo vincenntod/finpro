e734982997edbd896db656025bb9d0ecda43de3e
Arga Bimantara <63494224+argabima@users.noreply.github.com>
2023-07-11 10:06:11 +0700

    add

--- model/Connect.go
+++ model/Connect.go
@@ -3,28 +3,16 @@ package model
 import (
 	"fmt"
 
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 )
 
 var DB *gorm.DB
 
-func ConnectDatabase() {
-	dsn := "host=localhost user=postgres password=123456 dbname=dbceria port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
-		SingularTable: true,
-	}})
-	if err != nil {
-		fmt.Printf("Error")
-		return
-	}
-	DB = db
-}
-
 // func ConnectDatabase() {
-// 	dsn := "root:12345@tcp(localhost:3306)/dbceria?parseTime=true"
-// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
+// 	dsn := "host=localhost user=postgres password=12345 dbname=dbceria port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
 // 		SingularTable: true,
 // 	}})
 // 	if err != nil {
@@ -33,3 +21,15 @@ func ConnectDatabase() {
 // 	}
 // 	DB = db
 // }
+
+func ConnectDatabase() {
+	dsn := "root:12345@tcp(localhost:3306)/dbceria?parseTime=true"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
+		SingularTable: true,
+	}})
+	if err != nil {
+		fmt.Printf("Error")
+		return
+	}
+	DB = db
+}
