58a4446018f7d842aa1115a022261cd15d11b3fe
Vincencius Maxwell <vincennte@telkomuniversity.ac.id>
2023-07-14 04:54:05 +0700

    Delete connect mysql

--- model/Connect.go
+++ model/Connect.go
@@ -11,12 +11,6 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	// dsnmysqldeploy := "u472991515_vincenntes:vincen241!Aa@tcp(https://auth-db1031.hstgr.io/)/u472991515_dbceria?charset=utf8mb4&parseTime=True&loc=Local"
-	// dsnmysql := "root:123456@tcp(localhost:3307)/dbceria?charset=utf8mb4&parseTime=True&loc=Local"
-	// dsnpsql := "host=localhost user=postgres password=123456 dbname=dbceria port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	// db, err := gorm.Open(mysql.Open(dsnmysql), &gorm.Config{NamingStrategy: schema.NamingStrategy{
-	// 	SingularTable: true,
-	// }})
 
 	dsn := "host=localhost user=postgres password=123456 dbname=dbceria port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
